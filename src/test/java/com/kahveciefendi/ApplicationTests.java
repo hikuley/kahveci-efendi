da4a01c2c9e1ee9b1aef6f49b2d5d4693a5b1d27
hikuley <ibrahim.kuley@infoowl.com>
2017-09-25 08:38:16 +0300

    ++

--- src/test/java/com/kahveciefendi/ApplicationTests.java
+++ src/test/java/com/kahveciefendi/ApplicationTests.java
@@ -1,7 +1,11 @@
 package com.kahveciefendi;
 
+import com.kahveciefendi.entity.Product;
+import com.kahveciefendi.entity.ProductType;
+import com.kahveciefendi.repository.ProductRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+
+    @Autowired
+    ProductRepository productRepository;
+
+
+
+
+    @Test
+    public void contextLoads() {
+
+        Product product = new Product();
+        product.setName("test");
+        product.setDescription("testtest");
+        product.setProductType(ProductType.ADDONE);
+        product.setPrice(10.5);
+
+        productRepository.save(product);
+
+    }
 
 }
